727e9799a4379107b97bd562e1da07491ba03c4f
Fernando Romiti <fercabj@gmail.com>
2016-02-24 10:33:49 -0300

    Update Contents.swift

--- eChallenge_02/eChallenge_02.playground/Contents.swift
+++ eChallenge_02/eChallenge_02.playground/Contents.swift
@@ -20,8 +20,22 @@ class Calculator {
         }
         return sum
     }
+    
+    static func getSumForTheMultiplesOf3or5(count: Int) -> Int {
+        var sum = 0
+        for i in 1..<count {
+            if (i%3 == 0 || i%5 == 0) {
+                sum += i
+            }
+        }
+        return sum
+    }
 }
 
+//print(Calculator.getSumForTheMultiplesOf([3,5], forTheFirst: 10))
+//print(Calculator.getSumForTheMultiplesOf3and5(10))
 
-print(Calculator.getSumForTheMultiplesOf([3,5], forTheFirst: 10))
-print(Calculator.getSumForTheMultiplesOf([3,5], forTheFirst: 1000))
+print(Calculator.getSumForTheMultiplesOf([3,5], forTheFirst: 1000)) //Considering repetead numbers
+print(Calculator.getSumForTheMultiplesOf3or5(1000)) //No Repetead numbers considered
+
+// ;)
